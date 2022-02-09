bb902b68984f0ac6de9374b8f7979089871d5dc9
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-02-08 16:33:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 /*
- *  Project:    Versa 2 Dog Clock Face
+----------------------------------------------
+ *  Project:    Pet Space Dog Clock Face
  *  Mail:       darahbass@gmail.com
  *  Github:     SarahBass
- *  Credit:     volkan-labs reading-watch-face
- *  Credit:     mihaibabusca SnowflakeClockface
- *  Credit:     denk0403 Mario-Fitbit-Watchface
- */
+ ---------------------------------------------
+ NOTES: 
 
+ 
+ ---------------------------------------------
+*/
 
-import { geolocation } from "geolocation";
+/*--- Import Information from user Account ---*/
+import { settingsStorage } from "settings";
+import { me as appbit } from "appbit";
+import { HeartRateSensor } from "heart-rate";
 import clock from "clock";
 import * as document from "document";
 import { preferences } from "user-settings";
@@ -16,133 +21,362 @@ import * as util from "../common/utils";
 import { battery } from 'power';
 import { display } from "display";
 import { today as userActivity } from "user-activity";
+import {goals, today} from "user-activity";
 
-import { vibration } from "haptics";
-// Update the clock 
+
+/*--- Create Local Variables for Information Storage ---*/
+let daytext = "day";
+let monthtext = "month";
+let goalreached = "NONE";
+
+
+/*--- Import Information from index.gui ---*/
+
+let background = document.getElementById("background");
+let ampm = document.getElementById("ampm");  
+let date = document.getElementById("date");
+let hourhand = document.getElementById("hourhand");
+let minutehand = document.getElementById("minutehand");
+let minutehand2 = document.getElementById("minutehand2");
+let colon = document.getElementById("colon");
+let bodyobject = document.getElementById("bodyobject");
+let legs1object = document.getElementById("legs1object");
+let tailobject = document.getElementById("tailobject");
+let eyesobject = document.getElementById("eyesobject");
+let legs2object = document.getElementById("legs2object");
+let object = document.getElementById("object");
+let cuteobject = document.getElementById("cuteobject");
+let body = document.getElementById("body");
+let legs1 = document.getElementById("legs1");
+let eyes = document.getElementById("eyes");
+let legs2 = document.getElementById("legs2");
+let cute = document.getElementById("cute");
+let heartbutton = document.getElementById("heartbutton");  
+let heartclick = 0;   
+let pasteventread = "";   
+//Update the clock every second 
 clock.granularity = "seconds";
 
-//PLAYER
-let standLink;
-let jumpLink;
-let isJumping = false;
-let mario = document.getElementById("mario");
-let jumpAnim = document.getElementById("jump_animation");
-
-//SCENE
-let background = document.getElementById("background");
-let movable = document.getElementById("movable");
-
-// sunrise/sunset info
-let sunrise;
-let sunset;
-
-// Get a handle on the <text> element
+// Get a handle on the <text> elements 
 const myLabel = document.getElementById("myLabel");
-
 const batteryLabel = document.getElementById("batteryLabel");
-
-const greenbatteryLabel = document.getElementById("greenbatteryLabel");
-
-const redbatteryLabel = document.getElementById("redbatteryLabel");
-
 const stepsLabel = document.getElementById("stepsLabel");
+const firelabel = document.getElementById("firelabel");
+const boltlabel = document.getElementById("boltlabel");
+const stairslabel = document.getElementById("stairslabel");
+const distancelabel = document.getElementById("distancelabel");
+const targetlabel = document.getElementById("targetlabel");
+heartlabel.text = "off";
+ /*--- Animation Groups Imported from Index.gui---*/
+var demoinstance = document.getElementById("demoinstance");
 
+  
 
-
-// Update the <text> element every tick with the current time
+/*--- CLOCK START ---*/
 clock.ontick = (evt) => {
+
   let today = evt.date;
   let hours = today.getHours();
-  
-  checkAndUpdateBatteryLevel();
-  stepsLabel.text = userActivity.adjusted.steps;
- 
-  if (preferences.clockDisplay === "12h") {
-    // 12h format
-    hours = hours % 12 || 12;
-  } else {
-    // 24h format
-    hours = util.zeroPad(hours);
-  }
+  let months = today.getMonth();
+  let days = today.getDay();
+  let dates = today.getDate();
+  let years = today.getFullYear();
   let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+  let seconds = today.getSeconds();
   
-  if (hours === 0 && mins === 0) {
-  resetDate();}
+
+ /*--- Update Stats for Screen ---*/
   updateScene();
-}
+  
+  if (preferences.UnitsSettings == "us"){
+  distancelabel.text = (0.000621371 * userActivity.adjusted.distance).toFixed(1) + " mi";}
+  else {distancelabel.text = (0.001 * userActivity.adjusted.distance).toFixed(1) + " km";}
 
-display.addEventListener('change', function () {
-    if (this.on) {checkAndUpdateBatteryLevel();} 
+  stairslabel.text = userActivity.adjusted.elevationGain;
+  stepsLabel.text = userActivity.adjusted.steps;
+  firelabel.text = userActivity.adjusted.calories;
+  targetlabel.text = parseInt(userActivity.adjusted.steps/goals.steps * 100) + "%";
+  boltlabel.text = userActivity.adjusted.activeZoneMinutes.total;
+    
+  checkAndUpdateBatteryLevel();
+  
+  
+  //AM PM -Change the image based on 24 hours
+  if (util.zeroPad(hours) <12){ampm.image = "background/am.png";
+                              if ((util.zeroPad(hours) >= 0) && (util.zeroPad(hours) < 7)) {
+                                   cuteobject.image = "object/zzz.png";
+                                   cute.image = "object/zzz.png";}
+                              else if (util.zeroPad(hours) == 7){
+                                   cuteobject.image = "object/cereal.png";
+                                   cute.image = "object/cereal.png";}
+                              else if (util.zeroPad(hours) == 8){
+                                   cuteobject.image = "object/toothbrush.png";
+                                   cute.image = "object/toothbrush.png";}
+                              else if (util.zeroPad(hours) == 9){ 
+                                   cuteobject.image = "object/write.png"; 
+                                   cute.image = "object/write.png";}
+                              else if (util.zeroPad(hours) == 11){ 
+                                   cuteobject.image = "object/write.png"; 
+                                   cute.image = "object/write.png";}
+                              else {
+                                    cuteobject.image = " ";
+                                    cute.image = " ";}
+                              
+                              
+                              }
+  if (util.zeroPad(hours) >= 12){ampm.image = "background/pm.png";
+                                 
+                                 else if (util.zeroPad(hours) == 16){
+                                   cuteobject.image = "object/ballplay.png";
+                                   cute.image = "object/ballplay.png";}
+                                 else if (util.zeroPad(hours) == 17){  
+                                   cuteobject.image = "object/ballplay.png";
+                                   cute.image = "object/ballplay.png";}
+                                 else if (util.zeroPad(hours) == 18){  
+                                   cuteobject.image = "object/cereal.png";
+                                   cute.image = "object/cereal.png";}
+                                 else if (util.zeroPad(hours) == 19){  
+                                    cuteobject.image = "object/music.png";
+                                    cute.image = "object/music.png";} 
+                                 else if (util.zeroPad(hours) == 20){  
+                                    cuteobject.image = "object/movie.png";
+                                    cute.image = "object/movie.png";}
+                                 else if (util.zeroPad(hours) == 21){ 
+                                    cuteobject.image = "object/toothbrush.png";
+                                    cute.image = "object/toothbrush.png";}
+                                 else if ((util.zeroPad(hours) == 22) ||
+                                          (util.zeroPad(hours) == 23)){
+                                    cuteobject.image = "object/zzz.png";
+                                    cute.image = "object/zzz.png";}
+                                 else {
+                                    cuteobject.image = " ";
+                                    cute.image = " ";}
+                                 
+                                 }                            
+  
+  
+  //Get Prize from Steps Goal 
+  if (userActivity.adjusted.steps > goals.steps){goalreached = "show";}
+  
+  
+   /*--- OPTION 2: TIME IMAGES FOR 12 HOUR CLOCK---*/
+  //set class of each # IMAGE individually if needed for formatting
+  if (preferences.clockDisplay === "12h") {
+    colon.image = "minutesfile/colon.png";
+    myLabel.class = "myLabel";
+    hours = hours % 12 || 12;
+  
+  if (hours == 1){hourhand.image = "hoursfile/hour1.png";
+                  hourhand.class = "hour1";                   }
+  else if (hours == 2){hourhand.image = "hoursfile/hour2.png";
+                      hourhand.class = "hour";                }
+  else if (hours == 3){hourhand.image = "hoursfile/hour3.png";}
+  else if (hours == 4){hourhand.image = "hoursfile/hour4.png";}
+  else if (hours == 5){hourhand.image = "hoursfile/hour5.png";}
+  else if (hours == 6){hourhand.image = "hoursfile/hour6.png";}
+  else if (hours == 7){hourhand.image = "hoursfile/hour7.png";}
+  else if (hours == 8){hourhand.image = "hoursfile/hour8.png";}
+  else if (hours == 9){hourhand.image = "hoursfile/hour9.png";
+                        hourhand.class = "hour11";  }
+  else if (hours == 10){hourhand.image = "hoursfile/hour10.png";}
+  else if (hours == 11){hourhand.image = "hoursfile/hour11.png";
+                        hourhand.class = "hour11";}
+  else{hourhand.image = "hoursfile/hour12.png";
+                    hourhand.class = "hour12";}
+  
+  //Minute hand % 10 will return ones digit
+  if (mins%10 == 1 ){minutehand2.image =      "minutesfile/1.png";     
+                          minutehand2.class = "minute3";}
+  else if (mins%10 == 2 ){minutehand2.image = "minutesfile/2.png";
+                          minutehand2.class = "minute2";}
+  else if (mins%10 == 3 ){minutehand2.image = "minutesfile/3.png";
+                          minutehand2.class =  "minute2";}
+  else if (mins%10 == 4 ){minutehand2.image = "minutesfile/4.png";}
+  else if (mins%10 == 5 ){minutehand2.image = "minutesfile/5.png";}
+  else if (mins%10 == 6 ){minutehand2.image = "minutesfile/6.png";}
+  else if (mins%10 == 7 ){minutehand2.image = "minutesfile/7.png";}
+  else if (mins%10 == 8 ){minutehand2.image = "minutesfile/8.png";}
+  else if (mins%10 == 9 ){minutehand2.image = "minutesfile/9.png";}
+  else if (mins%10 == 0 ){minutehand2.image = "minutesfile/0.png";
+                          minutehand2.class = "minute2";}
+  else{minutehand2.image = " ";}
+  
+  //Minute hand /10 will return tens digit, but ints don't exist in Javascript
+  //Use the parseInt function to turn quotient into an integer
+       if (parseInt(mins/10) == 1 ){ minutehand.image = "minutesfile/1.png";
+                                     minutehand.class = "minute1";}         
+       if (parseInt(mins/10) == 2 ){ minutehand.image = "minutesfile/2.png";
+                                     minutehand.class = "minute"; }
+       if (parseInt(mins/10) == 3 ){ minutehand.image = "minutesfile/3.png";}
+       if (parseInt(mins/10) == 4 ){ minutehand.image = "minutesfile/4.png";}                                 
+       if (parseInt(mins/10) == 5 ){ minutehand.image = "minutesfile/5.png";}
+       if (parseInt(mins/10) == 6 ){ minutehand.image = "minutesfile/6.png";}
+       if (parseInt(mins/10) == 0 ){ minutehand.image = "minutesfile/0.png";}
+  } 
+    
+    /*--- OPTION 2: TIME TEXT FOR 24 HOUR CLOCK ---*/
+    //This is how to set a clock with text
+    //Invisible until 24 hour mode selected
+    else { 
+    hours = util.zeroPad(hours);
+    myLabel.text = `${hours}:${mins}`; 
+    myLabel.class = "showLabel";
+    hourhand.image = " ";
+    minutehand.image = " ";  
+    minutehand2.image = " ";
+    colon.image = " ";
+  }
+  
+  /*----------------------------SHOW CLOCK END----------------------------------*/
+  //ANIMATIONS
+
+  //PLAY FLOAT ANIMATION
+  if ( mins % 2 == 0){
+                         if (seconds % 2 == 0){mouth.image = "star/notongue.png";}
+                         else{mouth.image = "star/littlemouth.png";}     
+                         float();
+  //PLAY STAND ANIMATION - MOUTH CHANGES EVERY 10 MINUTES  
+  }else{            
+                   if ( parseInt(mins/10) == 1 ){
+                   if (seconds % 2 == 0){mouthobject.image = "star/littlemouth.png";}
+                   else{mouthobject.image = "star/tinymouth.png";}}
+                 else if (parseInt(mins/10) == 2 ){
+                   if (seconds % 2 == 0){mouthobject.image = "star/circlemouth.png";}
+                   else{mouthobject.image = "star/tinycirclemouth.png";}}
+                                     
+                 else if ( parseInt(mins/10) == 3 ){
+                   if (seconds % 2 == 0){mouthobject.image = "star/tinycirclemouth.png";}
+                   else{mouthobject.image = "star/mouth.png";}}  
+                 
+                 else if (parseInt(mins/10) == 4 ){ 
+                   if (seconds % 2 == 0){mouthobject.image = "star/littleovalmouth.png";}
+                   else{mouthobject.image = "star/tinymouth.png";}}
+                 
+                 else if (parseInt(mins/10) == 5 ){
+                   if (seconds % 2 == 0){mouthobject.image = "star/littlemouth.png";}
+                   else{mouthobject.image = "star/tinymouth.png";}}
+
+                 else if (parseInt(mins/10) == 6 ){ 
+                   if (seconds % 2 == 0){mouthobject.image = "star/notongue.png";}
+                   else{mouthobject.image = "star/littlemouth.png";}}
+                 
+                 else if (parseInt(mins/10) == 0 ){ 
+                   if (seconds % 2 == 0){mouthobject.image = "star/tinymouth.png";}
+                   else{mouthobject.image = "star/tinycirclemouth.png";}}
+                
+       stand();
+      }
+
+   
+
+  /*--- Battery Functions ---*/
+  display.addEventListener('change', function () { if (this.on) {checkAndUpdateBatteryLevel();}
+                                                  
+                                                    
+                                                  
 });
+/*----------------------------END OF ON TICK-----------------------------------*/
+  
+/*----------------------------START OF FUNCTIONS--------------------------------*/
 
-battery.onchange = (charger, evt) => {
-    greenBatteryLevel();
-}
-
-function greenBatteryLevel() {
-    greenbatteryLabel.text = `${battery.chargeLevel}%`;
-}
+ /*--- Change Battery RED , GREEN & CHARGE ---*/  
 
 function checkAndUpdateBatteryLevel() {
-  if (battery.chargeLevel > 15){
-    batteryLabel.text = `${battery.chargeLevel}%`;}
-  else {redbatteryLabel.text = `${battery.chargeLevel}%`;}
+  batteryLabel.text = `${battery.chargeLevel}%`;
+  if (battery.chargeLevel > 30){ batteryLabel.class = "labelgreen";}
+  else {batteryLabel.class = "labelred";
+        battery.onchange = (charger, evt) => {batteryLabel.class = "labelgreen";}}
 }
-
-function defaultSunsetSunrise(error) {
-  sunrise = new Date();
-  sunrise.setHours(6);
-  sunrise.setMinutes(0);
-  sunrise.setSeconds(0);
-  sunset = new Date();
-  sunset.setHours(20);
-  sunset.setMinutes(0);
-  sunset.setSeconds(0);
-}
-
-defaultSunsetSunrise(null);
-
-function setToNight() {
-  background.image = "batterydogscreen.png";
-}
-
-function setToMorning() {
-  background.image = "daydog.png";
-}
-
-function updateScene() {
-  let today = new Date();
-  if (today.getTime() >= sunrise.getTime() && today.getTime() < sunset.getTime()) {
-    setToMorning();
-  } else {
-    setToNight();
-  }
-}
-
-function updateSunsetSunrise(position) {
-  let loc = position.coords;
-  var times = answer.getTimes(new Date(), loc.latitude, loc.longitude);
-  sunrise = times.sunrise;
-  sunset = times.sunset;
-  updateScene();
-}
-
-function resetdog() {
-  if (isJumping) {
-    mario.image = jumpLink;
-  } else {
-    mario.image = standLink;
-  }
-}
-
-function setToRegular() {
-  jumpLink = "2jump.png";
-  standLink = "1jump.png";
-  resetdog();
+ 
   
+  
+/*--- Change Date and Background Functions ---*/
+  function updateScene() {
+   changeBackground();
+   date.text = " " + daytext + " " + monthtext + " " + dates + " " + years + " ";  
+  if (months == 0){monthtext = "January";}
+  else if (months == 1){monthtext =  "February";}
+  else if (months == 2){monthtext =  "March";}
+  else if (months == 3){monthtext =  "April";}
+  else if (months == 4){monthtext =  "May";}
+  else if (months == 5){monthtext =  "June";}
+  else if (months == 6){monthtext =  "July";}
+  else if (months == 7){monthtext =  "August";}
+  else if (months == 8){monthtext =  "Septemper";}
+  else if (months == 9){monthtext =  "October";}
+  else if (months == 10){monthtext = "November";}
+  else if (months == 11){monthtext = "December";}
+  else {monthtext = "MONTH";}
+    
+  if (days == 0){daytext =      "Sunday,";}
+  else if (days == 1){daytext = "Monday,";}
+  else if (days == 2){daytext = "Tuesday,";}
+  else if (days == 3){daytext = "Wednesday,";}
+  else if (days == 4){daytext = "Thursday,";}
+  else if (days == 5){daytext = "Friday,";}
+  else if (days == 6){daytext = "Saturday,";}
+  else {daytext = "DAY";}
+ }
+
+
+//You can use a convienent way to find your and upload your images
+//"file location" + number variable + ".imageformat" 
+
+ function changeBackground(){ 
+         
+   else {}  
 }
-function resetDate() {
-  geolocation.getCurrentPosition(updateSunsetSunrise, defaultSunsetSunrise);
+
+//Animation Functions   
+function float (){
+//if goif (goalreached == "show"){ 
+                           if ((months == 2)||(months == 3)){object.image = "object/mar" + (dates % 10) + ".png";}
+                           else if (months == 9){object.image = "object/oct" + (dates % 10) + ".png";}
+                           else if (months == 11){object.image = "object/dec" + (dates % 10) + ".png";}
+                           else {object.image = "object/plain" + (dates % 10) + ".png";}
+  } else{ object.image = " ";}
+  body.image = "dog/body.png"
+  eyes.image = "dog/eyes1.png";
+  legs1.image;
+  legs2.image;
+  tail.image;
+  cute.image;
+  bodyobject.image = "";
+  eyesobject.image = "";
+  legs1object.image = "";
+  legs2object.image = "";
+  tailobject.image = "";
+  cuteobject.image = "";
+  setTimeout(() => {
+  demoinstance.animate("enable"); 
+   }, 600);
+}  
+
+function stand(){
+  if (goalreached == "show"){ 
+                           if ((months == 2)||(months == 3)){object.image = "object/mar" + (dates % 10) + ".png";}
+                           else if (months == 9){object.image = "object/oct" + (dates % 10) + ".png";}
+                           else if (months == 11){object.image = "object/dec" + (dates % 10) + ".png";}
+                           else {object.image = "object/plain" + (dates % 10) + ".png";}
+  } else{ object.image = " ";}
+  
+  
+  body.image = " ";
+  eyes.image = " ";
+  legs1.image = " ";
+  legs2.image = " ";
+  tail.image = " ";
+  cute.image = " ";
+  bodyobject.image = "dog/body.png"
+  eyesobject.image = "dog/eyes1.png";
+  legs1object.image;
+  legs2object.image;
+  tailobject.image;
+  cuteobject.image;
+  
+}  
 }
+}
+/*----------------------------END OF FUNCTIONS--------------------------------*/
+/*-------------------------------END OF CODE----------------------------------*/
 
